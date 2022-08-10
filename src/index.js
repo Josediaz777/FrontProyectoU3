5592343d334506123183290ab6b8270faccae7b9
Josediaz777 <manueltiscareno1200@gmail.com>
2022-08-10 18:08:35 -0500

    Feat:We added the react router with the required routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from '../src/App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/login';
+import CVV from './components/cvv';
+import Acercade from './components/acercade';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cvv" element={<CVV />} />
+      <Route path="/acercade" element={<Acercade />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
