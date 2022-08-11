01e8618e04834d1795210f269dfb4ae1610532b1
Josediaz777 <manueltiscareno1200@gmail.com>
2022-08-11 03:06:53 -0500

    Feat:We added the about with the developers information

--- src/components/acercade.js
+++ src/components/acercade.js
@@ -1,37 +1,36 @@
 import React from 'react'
 import Navbar from './navbar';
 import foto from '../sources/yos.jpg'
+import foto2 from '../sources/alan.jpg'
 
 
 
 function Acercade() {
     return (
-        <div>
+        <div className="acercade">
             <Navbar />
-            <div className="acercade">
-                <div className='containerpok'>
-                    <div className="card2" style={{ width: '400px' }}>
-                        <div>
-                            <img src={foto} className="foto" alt="..." />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title">: Jose Manuel Tiscareño Moreno</h5>
-                            <p className="card-text">:191382</p>
-                        </div>
-
+            <div className='containerpok'>
+                <div className="card2" style={{ width: '450px' }}>
+                    <div>
+                        <img src={foto} className="foto" alt="..." />
                     </div>
+                    <div className="card-body">
+                        <h5 className="card-title">: Jose Manuel Tiscareño Moreno</h5>
+                        <p className="card-text">:191382</p>
+                    </div>
+
                 </div>
-                <div className='containerpok'>
-                    <div className="card2" style={{ width: '400px' }}>
-                        <div>
-                            <img src={foto} className="foto" alt="..." />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title">: Jose Manuel Tiscareño Moreno</h5>
-                            <p className="card-text">:191382</p>
-                        </div>
-
+            </div>
+            <div className='containerpok'>
+                <div className="card2" style={{ width: '450px' }}>
+                    <div>
+                        <img src={foto2} className="foto" alt="..." />
                     </div>
+                    <div className="card-body">
+                        <h5 className="card-title">: Alan Jesus Rivera Reyes</h5>
+                        <p className="card-text">:191338</p>
+                    </div>
+
                 </div>
             </div>
         </div>
